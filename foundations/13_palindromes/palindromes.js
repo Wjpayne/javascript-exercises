5a28f25d4cfdc1234206f5b5b200aa767b862ab9
Wjpayne <payne.j.will@gmail.com>
2025-12-17 12:34:10 -0600

    finish palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const cleanString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const reversedString = cleanString.split('').reverse().join('');
+  return cleanString === reversedString;
 };
 
 // Do not edit below this line
